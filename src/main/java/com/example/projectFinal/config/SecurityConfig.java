607c3d8dd5f4aa427c8ca94ef938248238b5733a
symbicort <jeongwonlee0@gmail.com>
2024-03-17 01:10:39 +0900

    회원가입 로직 생성

--- src/main/java/com/example/projectFinal/config/SecurityConfig.java
+++ src/main/java/com/example/projectFinal/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,7 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth.requestMatchers("/").permitAll());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/user/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
@@ -21,5 +27,4 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 }
--- src/main/java/com/example/projectFinal/controller/UserController.java
+++ src/main/java/com/example/projectFinal/controller/UserController.java
@@ -1,11 +1,28 @@
 package com.example.projectFinal.controller;
 
+import com.example.projectFinal.dto.UserDto;
+import com.example.projectFinal.entity.User;
+import com.example.projectFinal.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-//    유저 컨트롤러
+    //    유저 컨트롤러
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody UserDto.RegisterDto registerDto){
+        User result = this.userService.SignUp(registerDto);
+        System.out.println(result);
+
+    }
 
 }

968b058cf9b50e4268a807eb0a851afe838a524a
seyeonsim <seyeonsimw@gmail.com>
2024-03-30 12:01:09 +0900

    send missions

--- src/main/java/com/example/projectFinal/controller/UserMissionController.java
+++ src/main/java/com/example/projectFinal/controller/UserMissionController.java
@@ -4,10 +4,8 @@ import com.example.projectFinal.dto.UserMissionDto;
 import com.example.projectFinal.entity.UserMissionEntity;
 import com.example.projectFinal.service.UserMissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +14,35 @@ public class UserMissionController {
     @Autowired
     private  UserMissionService userMissionService;
 
+//    @PostMapping("/course")
+//    public void addUserMissionsForCourse(@RequestParam String course, @RequestParam String userId) {
+//        userMissionService.addUserMissionsForCourse(course, userId);
+//    }
+
     @PostMapping("/course")
-    public void addUserMissionsForCourse(@RequestParam String course, @RequestParam String userId) {
-        userMissionService.addUserMissionsForCourse(course, userId);
+    public void addUserMissionsForCourse(@RequestParam String course,
+                                         @CookieValue(name = "accessToken", required = false) String accessToken,
+                                         @CookieValue(name = "RefreshToken", required = false) String RefreshToken) {
+        userMissionService.addUserMissionsForCourse(course, accessToken, RefreshToken);
     }
 
+//    @GetMapping("/missions")
+//    public List<UserMissionDto> getUnusedMissionsForUser(@RequestParam String userId) {
+//        return userMissionService.getUnusedMissionsForUser(userId);
+//    }
+
     @GetMapping("/missions")
-    public List<UserMissionDto> getUnusedMissionsForUser(@RequestParam String userId) {
-        return userMissionService.getUnusedMissionsForUser(userId);
+    public List<UserMissionDto> getUnusedMissionsForUser(@CookieValue(name = "accessToken", required = false) String accessToken,
+                                                         @CookieValue(name = "RefreshToken", required = false) String RefreshToken) {
+        return userMissionService.getUnusedMissionsForUser(accessToken, RefreshToken);
     }
+
+//    @CrossOrigin
+//    @PostMapping("/checkMission")
+//    public ResponseEntity<?> checkMission(@RequestBody String postData) {
+//        System.out.println("Received data:");
+//        System.out.println(postData);
+//
+//        return ResponseEntity.ok().build();
+//    }
 }

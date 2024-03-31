479997a82ac996b65f5235b125c91ae9ee2a1428
seyeonsim <seyeonsimw@gmail.com>
2024-03-31 10:38:19 +0900

    learned mission

--- src/main/java/com/example/projectFinal/entity/UserMissionEntity.java
+++ src/main/java/com/example/projectFinal/entity/UserMissionEntity.java
@@ -1,12 +1,10 @@
 package com.example.projectFinal.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
